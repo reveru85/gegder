9fba75e3c3dbdea42fa06c7525a25b9142a95326
reveru85 <Saphia@Saphias-MacBook-Pro.local>
2015-06-26 00:48:14 +0800

    Completed reloading of previous posts

--- Gegder/Gegder/HomeViewController.swift
+++ Gegder/Gegder/HomeViewController.swift
@@ -14,18 +14,19 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     let postCellId = "PostCell"
     let data = PostData()
     let userID = (UIApplication.sharedApplication().delegate as! AppDelegate).userID
-    var firstLoadComplete = false
+    var lastPostID = ""
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
         HomeTableView.delegate = self
         HomeTableView.dataSource = self
         HomeTableView.estimatedRowHeight = 44
         HomeTableView.rowHeight = UITableViewAutomaticDimension
         
-        // Get First Load Posts API
+        // Get first load posts
         var urlString = "http://dev.snapsnap.com.sg/index.php/dphodto/dphodto_list/" + userID!
         let url = NSURL(string: urlString)
         var request = NSURLRequest(URL: url!)
@@ -33,40 +34,38 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
             if data != nil {
                 var posts = JSON(data: data!)
-                
-                //first load clear placeholder entry
                 self.data.clearEntries()
                 self.data.addEntriesFromJSON(posts)
                 self.HomeTableView.reloadData()
             }
         })
-        
     }
     
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return data.entries.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier(postCellId, forIndexPath: indexPath) as! PostTableViewCell
         
 //        cell.PostCommentButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
 //        cell.PostLikeButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
 //        cell.PostDislikeButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
         
-        //post contains post data
+        // Initialise an instance of PostData class using the current row
         let post = data.entries[indexPath.row]
         
-        //username
+        // Update the UI with the current post
         cell.UserLabel.text = post.username
-        //location
         cell.UserLocation.text = post.location
-        //async image load
+    
         if post.media_url != nil {
             if let imageUrl = NSURL(string: post.media_url!) {
                 let imageRequest: NSURLRequest = NSURLRequest(URL: imageUrl)
@@ -80,50 +79,52 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
                 })
             }
         }
+        
         cell.PostDateTime.text = post.created_datetime
         cell.PostHashtags.text = post.hash_tag
         cell.PostCommentCount.text = post.total_comments
         cell.PostLikeCount.text = post.total_likes
         cell.PostDislikeCount.text = post.total_dislikes
         
-        // Update rows remaining based on the currently loaded row
-//        println("\(data.entries.count) \(indexPath.row)")
-//        let rowsRemaining = data.entries.count - indexPath.row
-//        if rowsRemaining < 3 {
-//            println("Reload now: \(rowsRemaining)")
-//        }
-        
         return cell
-    }
-
-    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-        
-        if firstLoadComplete {
-            println(indexPath.row)
-            if indexPath.row + 1 == data.entries.count {
-                println("TRIGGER")
-                
-//                // Get First Load Posts API
-//                var urlString = "http://dev.snapsnap.com.sg/index.php/dphodto/dphodto_list/" + userID!
-//                let url = NSURL(string: urlString)
-//                var request = NSURLRequest(URL: url!)
-//                let queue: NSOperationQueue = NSOperationQueue.mainQueue()
-//                NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-//                    if data != nil {
-//                        var posts = JSON(data: data!)
-//                        
-//                        //first load clear placeholder entry
-//                        self.data.clearEntries()
-//                        self.data.addEntriesFromJSON(posts)
-//                        self.HomeTableView.reloadData()
-//                    }
-//                })
-            }
-        }
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
-        firstLoadComplete = true;
+        
+        var height = scrollView.frame.size.height
+        var contentYoffset = scrollView.contentOffset.y
+        var distanceFromBottom = scrollView.contentSize.height - contentYoffset
+        
+        if distanceFromBottom < height {
+            // Reached end of table
+            getPreviousPosts(data.entries.last?.post_id!)
+        }
+    }
+    
+    func getPreviousPosts(postID : String?) {
+        
+        // Track the last post ID globally so as to not make repeated "Get Previous Posts" on the same last post
+        if lastPostID != postID {
+            lastPostID = postID!
+            
+            // Get Previous Posts based on Oldest Post
+            var urlString = "http://dev.snapsnap.com.sg/index.php/dphodto/dphodto_previous_post/" + postID! + "/" + userID!
+            let url = NSURL(string: urlString)
+            var request = NSURLRequest(URL: url!)
+            let queue: NSOperationQueue = NSOperationQueue.mainQueue()
+            NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                if data != nil {
+                    var posts = JSON(data: data!)
+                    println("There are \(posts.count) new posts")
+                    
+                    // Only add if JSON from server contains more posts
+                    if posts.count != 0 {
+                        self.data.addEntriesFromJSON(posts)
+                        self.HomeTableView.reloadData()
+                    }
+                }
+            })
+        }
     }
 }
 
